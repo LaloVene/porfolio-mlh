076590d3a7f76bf8f46611e490de1107ccc105d5
Eduardo Venegas <lalohdez77@gmail.com>
2021-07-08 12:30:31 -0500

    fix tput error

--- app/prod-test.sh
+++ app/prod-test.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-TERM=xterm
 server="https://lalovene.duckdns.org/"
 while read endpoint;
 do
 response=$(curl --write-out '%{http_code}' --silent --output /dev/null $server$endpoint)
 if [[ $response == "200" ]]; then
-tput setaf 2; echo "Success /"$endpoint
+echo "Success /"$endpoint
 else
-tput setaf 1; echo "Error /"$endpoint
+echo "Error /"$endpoint
 exit 1
 fi
-tput sgr0
+
 done < endpoint.txt
 exit 0
